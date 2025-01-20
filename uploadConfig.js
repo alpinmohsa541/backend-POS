99e5b4da6c53c95ef3990eca53d8bd972ffc63f9
Alpin Apriliansyah Mohsa <55131607+alpinmohsa541@users.noreply.github.com>
2025-01-20 10:57:05 +0700

    Update uploadConfig.js

--- uploadConfig.js
+++ uploadConfig.js
@@ -1,22 +1,8 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
 
-// Gunakan direktori yang dapat ditulis
-const dir = "assets/";
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, dir); // Direktori yang dapat ditulis
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname.replace(/\s+/g, "_");
-    cb(null, `${Date.now()}-${fileName}`);
-  },
-});
+// Konfigurasi storage menggunakan memory storage
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
@@ -33,7 +19,7 @@ const upload = multer({
       cb(new Error("Error: Images Only!"));
     }
   },
-  limits: { fileSize: 1000000 },
+  limits: { fileSize: 1000000 }, // Limit file size to 1MB
 });
 
 module.exports = upload;
